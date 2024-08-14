c6ee75269a5086e7373ce80f5053b4cdcd491461
Auto Logon <autologon@CSED.LAB>
2024-08-14 11:26:46 -0300

    mais

--- ListaExercios/Exe06a/Program.cs
+++ ListaExercios/Exe06a/Program.cs
@@ -13,3 +13,13 @@ for (int i = 0; i < vetor.Length; i++)
     
 Console.Write(vetor[i] + " ");
 }
+
+for (int i = 0; i < vetor.Length - 1; i++)
+{
+    if(vetor[i] > vetor[i + 1])
+    {
+        int aux = vetor[i];
+        vetor[i] = vetor[i + 1];
+        vetor[i + 1] = aux;
+    }
+}
